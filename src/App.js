1903472179be691008d0cbd0aa579d199bff8e4b
georgespicy <kolawolegeorge28743@gmail.com>
2023-04-07 15:07:36 +0100

    autocomplete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Test4 from './components/Test4';
+import Test5 from './components/Test5';
 
 function App() {
   return (
     <>
-      <Test4 />
+      <Test5 />
     </>
   );
 }
---
+++ src/components/Test5.js
@@ -0,0 +1,56 @@
+import { Autocomplete, Box, TextField } from "@mui/material";
+import React from "react";
+
+function Test5() {
+  const state = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttarakhand",
+    "Uttar Pradesh",
+    "West Bengal",
+    "Andaman and Nicobar Islands",
+    "Chandigarh",
+    "Dadra and Nagar Haveli",
+    "Daman and Diu",
+    "Delhi",
+    "Lakshadweep",
+    "Puducherry",
+  ];
+  return (
+    <Box>
+      <Autocomplete
+        sx={{ width: 300, margin: "auto" }}
+        options={state}
+        renderInput={(params) => (
+          <TextField {...params} label="list of states" />
+        )}
+      />
+    </Box>
+  );
+}
+
+export default Test5;
