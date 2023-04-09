fad033f825a58e2549dbcac2bb4a1ef6682cdb8e
georgespicy <kolawolegeorge28743@gmail.com>
2023-04-09 14:28:33 +0100

    building a navbar

--- src/components/Test7.js
+++ src/components/Test7.js
@@ -1,9 +1,12 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 function Test7() {
   return (
     <div>
-      
+      <Grid container>
+        <Grid></Grid>
+      </Grid>
     </div>
   )
 }
